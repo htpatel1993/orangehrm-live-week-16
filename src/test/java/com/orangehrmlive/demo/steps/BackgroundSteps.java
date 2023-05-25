72ff1685954085642aabd70e10268b226cc4fd28
htpatel.HimanshuPatel <htpatel1993@gmail.com>
2023-05-25 23:16:27 +0100

    Week 16

---
+++ src/test/java/com/orangehrmlive/demo/steps/BackgroundSteps.java
@@ -0,0 +1,14 @@
+package com.orangehrmlive.demo.steps;
+
+import cucumber.api.java.en.Given;
+
+/**
+ * Created By Kashyap patel
+ */
+public class BackgroundSteps {
+
+    @Given("^I am Login Page$")
+    public void iAmLoginPage() {
+    }
+
+}
